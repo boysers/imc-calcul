e55b2fe320975bfe3bb8c6a3c933f38f46d90ef4
boysers <boyser2015@gmail.com>
2021-06-08 00:47:58 +0200

    add erreur message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* ----- Les Variables ----- */
 let taille = "";
 let poids = "";
+let yourImc = "";
 
 /* ----- Les Sélecteurs ----- */
 let inputTaille = document.querySelector("#inputTaille");
@@ -9,11 +10,11 @@ let btnCalc = document.querySelector("#btnCalc");
 let userImc = document.querySelector("#userImc");
 
 /* ----- Les Functions ----- */
-// Calcul imc: t = taille en cm, p = poids en kg et n convertit la taille en mètre
+// Calcul imc: t = taille en cm, p = poids en kg et m convertit la taille en mètre
 function imc(t, p) {
-    // Formule: Imc = Poids / Taille²
-    let n = t / 100;
-    let r = p / (n * n);
+    // Formule: Imc = Poids / Taille²(m)
+    let m = t / 100;
+    let r = p / (m * m);
     return r.toFixed(1);
 }
 
@@ -27,7 +28,7 @@ function addText(element, message, valeur) {
         element.style.color = "#FF8080";
         element.style.background = "#290000";
     } else {
-        element.innerHTML = message ? message + valeur : "Correct " + valeur;
+        element.innerHTML = message ? message : "Correct";
         element.style.color = "";
         element.style.background = "";
     }
@@ -39,17 +40,24 @@ function inputImc() {
     taille = parseInt(inputTaille.value);
     poids = parseInt(inputPoids.value);
 
-    // Regarde si les valeurs sont vraie ou fausse
-    // "                         " inferieur à 0
-    if (!taille || !poids || taille < 0 || poids < 0) {
-        inputTaille.value = "";
-        inputPoids.value = "";
+    yourImc = imc(taille, poids)
 
-        // Ajout de null pour dire que la valeur est vide
-        addText(userImc, "Taille ou poids invalide", null);
-        return;
+    switch (true) {
+        case !taille && !poids:
+            addText(userImc, "Taille et poids ne sont pas définis", false);
+            break
+        case !taille:
+            addText(userImc, "Taille n'est pas définie", false);
+            break
+        case !poids:
+            addText(userImc, "Poids n'est pas définie", false);
+            break
+        case yourImc > 61 || yourImc < 7:
+            addText(userImc, "L'imc est incorrect", false);
+            break
+        default : 
+            addText(userImc, "Votre Imc est de " + yourImc, true);
     }
-    addText(userImc, "Votre imc est de ", imc(taille, poids));
 }
 
 /* ----- Les Ecouteurs ----- */
